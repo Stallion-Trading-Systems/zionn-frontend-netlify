44a1bce9e343d58a6f086f6abc131ca3ce0d6a7c
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2022-09-24 16:10:02 +0530

    sellbuyreq check

--- src/Containers/SellBuyRequest/SellBuyRequest.jsx
+++ src/Containers/SellBuyRequest/SellBuyRequest.jsx
@@ -27,6 +27,7 @@ const SellBuyRequest = (props) => {
     const [price, setPrice] = useState(0);
     const [minmaxError, setminmaxError] = useState(false);
     const [errorprice, seterrorprice] = useState(false);
+    const [loading, setLoading] = useState(false);
     const handleClick = (event) => {
         setIsActive((current) => !current);
     };
@@ -35,8 +36,18 @@ const SellBuyRequest = (props) => {
     };
     const createrequestsubmit = async (e) => {
         e.preventDefault();
-        if (maxVal < minVal || !price) {
-            if (maxVal < minVal) {
+
+        let mnv, mxv;
+        mnv = minVal;
+        mxv = maxVal;
+        if (mnv == 0 && mxv != 0) {
+            mnv = mxv;
+        }
+        if (mxv == 0 && mnv != 0) {
+            mxv = mnv;
+        }
+        if (mxv < mnv || !price) {
+            if (mxv < mnv) {
                 setminmaxError(true);
 
             }
@@ -46,9 +57,12 @@ const SellBuyRequest = (props) => {
             }
             return;
         }
-        let res = await api.sellbuyreq({ c_name: props.cname, min_val: minVal, max_val: maxVal, trans_price: price, trans_type: transType, email: userobj.email })
+        setLoading(true);
+        let res = await api.sellbuyreq({ c_name: props.cname, min_val: mnv, max_val: mxv, trans_price: price, trans_type: transType, email: userobj.email })
         console.log(res);
+        setLoading(false);
         setDone(true);
+
     }
 
     return (
@@ -78,54 +92,48 @@ const SellBuyRequest = (props) => {
                                     <p className='sellbuyreq-con-css'>Create Indication of Interest </p>
                                     <p className='sellbuyreq-con-des-css'>Submit this form to express the terms that you would consider for a transaction. IOIs are non-binding and not a commitment to trade. If you need help, contact your specialist.</p>
                                     <br />
-                                    
+
                                     {done ? <><div class="alert alert-success" role="alert">
                                         your response is saved
                                     </div></> :
                                         <>
-                                            <form onSubmit={createrequestsubmit} id="req-form" >
-                                            <p className='sellbuyreq-con-css'>Order type</p>
-                                                <div className='d-flex justify-content-center align-items-center'>
-                                                    <div className='wrapper '>
-                                                        <input checked={transType == "buy"} className='input-display-none' onChange={(e) => { setTansType(e.target.value) }} type="radio" name="buy" value="buy" id="option-1" />
-                                                        <input checked={transType == "sell"} className='input-display-none' onChange={(e) => { setTansType(e.target.value) }} type="radio" name="sell" value="sell" id="option-2" />
-                                                        <label for="option-1" class="option option-b option-1">
-                                                            <span>buy</span>
-                                                        </label>
-                                                        <label for="option-2" class="option option-s option-2">
-                                                            <span>sell</span>
-                                                        </label>
-                                                    </div>
+                                            {loading ? <>
+                                                <div class="alert alert-warning" role="alert">
+                                                    we are processing your request
                                                 </div>
-                                                <p className='mt-4 sellbuyreq-con-css mb-4'>Number of equity shares</p>
-                                                <div className='container'>
-
-                                                    <div className='row'>
-                                                        <div className='col-6'>
-                                                            <p className='label-input-css-sellbuyreq'>Min.</p>
-                                                            <input className='sellbuyreq-input-css' min="1" onChange={(e) => { setMinVal(e.target.value); if(maxVal==0)setMaxVal(e.target.value); setminmaxError(false) }} type="number" />
-                                                        </div>
-                                                        <div className='col-6'>
-                                                            <p className='label-input-css-sellbuyreq'>Max.</p>
-                                                            <input className='sellbuyreq-input-css' min="1" onChange={(e) => { setMaxVal(e.target.value); if(minVal==0)setMinVal(e.target.value); setminmaxError(false) }} type="number" />
+                                            </> : <>
+                                                <form onSubmit={createrequestsubmit} id="req-form" >
+                                                    <p className='sellbuyreq-con-css'>Order type</p>
+                                                    <div className='d-flex justify-content-center align-items-center'>
+                                                        <div className='wrapper '>
+                                                            <input checked={transType == "buy"} className='input-display-none' onChange={(e) => { setTansType(e.target.value) }} type="radio" name="buy" value="buy" id="option-1" />
+                                                            <input checked={transType == "sell"} className='input-display-none' onChange={(e) => { setTansType(e.target.value) }} type="radio" name="sell" value="sell" id="option-2" />
+                                                            <label for="option-1" class="option option-b option-1">
+                                                                <span>buy</span>
+                                                            </label>
+                                                            <label for="option-2" class="option option-s option-2">
+                                                                <span>sell</span>
+                                                            </label>
                                                         </div>
                                                     </div>
-                                                    {minmaxError && <div className='row'>
-                                                        <p style={{ color: "red" }}>* max cannot be less than min</p>
-                                                    </div>}
-                                                </div>
-                                                {transType == "buy" ? <><p className='mt-5 mb-3 sellbuyreq-con-css'>Bid Price</p>
-
+                                                    <p className='mt-4 sellbuyreq-con-css mb-4'>Number of equity shares</p>
                                                     <div className='container'>
+
                                                         <div className='row'>
                                                             <div className='col-6'>
-                                                                <input className='sellbuyreq-input-css' onChange={(e) => { setPrice(e.target.value); seterrorprice(false) }} min="0" type="number" required />
+                                                                <p className='label-input-css-sellbuyreq'>Min.</p>
+                                                                <input className='sellbuyreq-input-css' min="1" onChange={(e) => { setMinVal(e.target.value); setminmaxError(false) }} type="number" />
                                                             </div>
-
+                                                            <div className='col-6'>
+                                                                <p className='label-input-css-sellbuyreq'>Max.</p>
+                                                                <input className='sellbuyreq-input-css' min="1" onChange={(e) => { setMaxVal(e.target.value); setminmaxError(false) }} type="number" />
+                                                            </div>
                                                         </div>
-                                                    </div></> :
-                                                    <>
-                                                        <p className='mt-5 mb-3 sellbuyreq-con-css'>Ask Price</p>
+                                                        {minmaxError && <div className='row'>
+                                                            <p style={{ color: "red" }}>* max cannot be less than min</p>
+                                                        </div>}
+                                                    </div>
+                                                    {transType == "buy" ? <><p className='mt-5 mb-3 sellbuyreq-con-css'>Bid Price</p>
 
                                                         <div className='container'>
                                                             <div className='row'>
@@ -134,33 +142,46 @@ const SellBuyRequest = (props) => {
                                                                 </div>
 
                                                             </div>
-                                                        </div>
-                                                    </>
-                                                }
-                                                {errorprice && <p style={{ color: "red" }} >*enter price</p>}
-                                                <div className='container'>
-                                                    <div className='row'>
-                                                        <div className='col-4'></div>
-                                                        <div className='col-4'>
-                                                            <div className='mt-5 mb-4 d-flex justify-content-center align-items-center'>
-                                                                <button
+                                                        </div></> :
+                                                        <>
+                                                            <p className='mt-5 mb-3 sellbuyreq-con-css'>Ask Price</p>
 
-                                                                    onPointerLeave={defaultClick}
-                                                                    onPointerDown={handleClick}
-                                                                    onPointerUp={handleClick}
-                                                                    onClick={createrequestsubmit}
-                                                                    className={isActive ? "butt butt-ac" : "butt"}
-                                                                >
-                                                                    create request
-                                                                    <i class="bi bi-arrow-up-right"></i>
-                                                                </button>
+                                                            <div className='container'>
+                                                                <div className='row'>
+                                                                    <div className='col-6'>
+                                                                        <input className='sellbuyreq-input-css' onChange={(e) => { setPrice(e.target.value); seterrorprice(false) }} min="0" type="number" required />
+                                                                    </div>
+
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                        <div className='col-4'></div>
-                                                    </div>
+                                                        </>
+                                                    }
+                                                    {errorprice && <p style={{ color: "red" }} >*enter price</p>}
+                                                    <div className='container'>
+                                                        <div className='row'>
+                                                            <div className='col-4'></div>
+                                                            <div className='col-4'>
+                                                                <div className='mt-5 mb-4 d-flex justify-content-center align-items-center'>
+                                                                    <button
 
-                                                </div>
-                                            </form></>
+                                                                        onPointerLeave={defaultClick}
+                                                                        onPointerDown={handleClick}
+                                                                        onPointerUp={handleClick}
+                                                                        onClick={createrequestsubmit}
+                                                                        className={isActive ? "butt butt-ac" : "butt"}
+                                                                    >
+                                                                        create request
+                                                                        <i class="bi bi-arrow-up-right"></i>
+                                                                    </button>
+                                                                </div>
+                                                            </div>
+                                                            <div className='col-4'></div>
+                                                        </div>
+
+                                                    </div>
+                                                </form>
+                                            </>}
+                                        </>
                                     }
                                 </div>
                             </div>
