1d9b82e06d689f5d261f9aa5cea11de1d0555070
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2022-10-13 19:06:12 +0530

    staging check

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 
 const API = axios.create({
-    // baseURL: 'https://zionn-stage.herokuapp.com/'
-    baseURL: 'https://zionn-prod.herokuapp.com/'
+    baseURL: 'https://zionn-stage.herokuapp.com/'
+    // baseURL: 'https://zionn-prod.herokuapp.com/'
     // baseURL: 'http://localhost:8080/'
 
 })
